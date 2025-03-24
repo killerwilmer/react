7fdf0e16e5e538247efa864224702a35f698909b
Wilmer Arteaga <killerwilmer@gmail.com>
2025-03-24 14:58:29 -0500

    Validate response call

--- react-vite-example/src/componets/UserListLoading/UserListLoading.tsx
+++ react-vite-example/src/componets/UserListLoading/UserListLoading.tsx
@@ -11,6 +11,11 @@ const UserListLoading = () => {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/users"
         );
+        
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
         const data = await response.json();
         setUsers(data);
         setIsLoading(false);
